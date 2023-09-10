8bf8b5f7f84adfd84cbf10628a46090e01262536
deanOfWalls <deanofwalls@gmail.com>
2023-09-10 08:35:31 -0400

    modified eventlistener for nextButton

--- script.js
+++ script.js
@@ -25,61 +25,69 @@ fetch('questions.json')
         console.error('Error loading quiz data:', error);
     });
 
-    nextButton.addEventListener('click', () => {
-        const selectedAnswer = document.querySelector('input[name="answer"]:checked');
-        if (!selectedAnswer) return;
-    
-        const answerCorrect = selectedAnswer.value === 'true' || selectedAnswer.value === true; // Compare as boolean
-    
-        if (answerCorrect) {
-            score++;
-            showScore();
-            correctAnswers.push(shuffledQuestions[currentQuestionIndex].question);
-            showFeedback('Correct!', 'green');
-        } else {
-            incorrectAnswers.push(shuffledQuestions[currentQuestionIndex].question);
-            showFeedback('Incorrect!', 'red');
-        }
-        totalAnswered++; // Increment the total answered questions
-        currentQuestionIndex++;
-        showNextQuestion();
-    });
-    
+nextButton.addEventListener('click', () => {
+    const selectedAnswer = document.querySelector('input[name="answer"]:checked');
+    if (!selectedAnswer) return;
 
-// Rename the "Finish" button to "Recap"
-recapButton.textContent = 'Recap';
+    const answerCorrect = selectedAnswer.value === 'true' || selectedAnswer.value === true; // Compare as boolean
 
-recapButton.addEventListener('click', () => {
-    recapQuiz();
+    if (answerCorrect) {
+        score++;
+        showScore();
+        correctAnswers.push(shuffledQuestions[currentQuestionIndex].question);
+        showFeedback('Correct!', 'green');
+    } else {
+        incorrectAnswers.push(shuffledQuestions[currentQuestionIndex].question);
+        showFeedback('Incorrect!', 'red');
+    }
+    totalAnswered++; // Increment the total answered questions
+    currentQuestionIndex++;
+    showNextQuestion();
 });
 
+// Rename the "Finish" button to "Recap"
+if (recapButton) {
+    recapButton.textContent = 'Recap';
+    recapButton.addEventListener('click', () => {
+        recapQuiz();
+    });
+}
+
 function showFeedback(text, color) {
-    previousAnswer.textContent = `Previous answer: ${text}`;
-    previousAnswer.style.color = color;
+    if (previousAnswer) {
+        previousAnswer.textContent = `Previous answer: ${text}`;
+        previousAnswer.style.color = color;
+    }
 }
 
 function showScore() {
-    const percentage = ((score / totalAnswered) * 100).toFixed(2); // Calculate score percentage
-    scoreDisplay.textContent = `Score: ${percentage}%`;
+    if (scoreDisplay) {
+        const percentage = ((score / totalAnswered) * 100).toFixed(2); // Calculate score percentage
+        scoreDisplay.textContent = `Score: ${percentage}%`;
+    }
 }
 
 function showNextQuestion() {
     const selectedAnswer = document.querySelector('input[name="answer"]:checked');
 
     if (selectedAnswer) {
-        const answerCorrect = selectedAnswer.value === 'true';
+        const answerCorrect = selectedAnswer.value === 'true' || selectedAnswer.value === true; // Compare as boolean
         if (answerCorrect) {
             showFeedback('Correct!', 'green');
         } else {
             showFeedback('Incorrect!', 'red');
         }
     } else {
-        previousAnswer.textContent = '';
+        if (previousAnswer) {
+            previousAnswer.textContent = '';
+        }
     }
 
     if (currentQuestionIndex < shuffledQuestions.length) {
         showQuestion(shuffledQuestions[currentQuestionIndex]);
-        questionNumber.textContent = `${currentQuestionIndex + 1}/${shuffledQuestions.length}`;
+        if (questionNumber) {
+            questionNumber.textContent = `${currentQuestionIndex + 1}/${shuffledQuestions.length}`;
+        }
         showScore();
     } else {
         showResult();
@@ -87,46 +95,52 @@ function showNextQuestion() {
 }
 
 function showQuestion(question) {
-    questionNumber.innerText = `Question ${currentQuestionIndex + 1}`;
-    questionContainer.innerText = question.question;
-    answerForm.innerHTML = '';
+    if (questionContainer) {
+        questionNumber.innerText = `Question ${currentQuestionIndex + 1}`;
+        questionContainer.innerText = question.question;
+        answerForm.innerHTML = '';
 
-    // Shuffle the answers randomly
-    const shuffledAnswers = question.answers.sort(() => Math.random() - 0.5);
+        // Shuffle the answers randomly
+        const shuffledAnswers = question.answers.sort(() => Math.random() - 0.5);
 
-    shuffledAnswers.forEach((answer, index) => {
-        const input = document.createElement('input');
-        input.type = 'radio';
-        input.name = 'answer';
-        input.value = answer.correct;
-        input.id = `answer${index}`;
-        const label = document.createElement('label');
-        label.htmlFor = `answer${index}`;
-        label.innerText = answer.text;
-        answerForm.appendChild(input);
-        answerForm.appendChild(label);
-        answerForm.appendChild(document.createElement('br'));
-    });
+        shuffledAnswers.forEach((answer, index) => {
+            const input = document.createElement('input');
+            input.type = 'radio';
+            input.name = 'answer';
+            input.value = answer.correct;
+            input.id = `answer${index}`;
+            const label = document.createElement('label');
+            label.htmlFor = `answer${index}`;
+            label.innerText = answer.text;
+            answerForm.appendChild(input);
+            answerForm.appendChild(label);
+            answerForm.appendChild(document.createElement('br'));
+        });
+    }
 }
 
 function showResult() {
-    result.innerHTML = `
-        <h2>Your Score: ${score}</h2>
-        <h3>Correct Answers:</h3>
-        <ul>${correctAnswers.map(q => `<li>${q}</li>`).join('')}</ul>
-        <h3>Incorrect Answers:</h3>
-        <ul>${incorrectAnswers.map(q => `<li>${q}</li>`).join('')}</ul>
-    `;
+    if (result) {
+        result.innerHTML = `
+            <h2>Your Score: ${score}</h2>
+            <h3>Correct Answers:</h3>
+            <ul>${correctAnswers.map(q => `<li>${q}</li>`).join('')}</ul>
+            <h3>Incorrect Answers:</h3>
+            <ul>${incorrectAnswers.map(q => `<li>${q}</li>`).join('')}</ul>
+        `;
+    }
 }
 
 // Function to display incorrectly answered questions
 function recapQuiz() {
-    const recapContainer = document.getElementById('result');
-    recapContainer.innerHTML = '<h2>Recap of Incorrect Answers</h2>';
+    if (result) {
+        const recapContainer = result;
+        recapContainer.innerHTML = '<h2>Recap of Incorrect Answers</h2>';
     
-    const uniqueIncorrectAnswers = [...new Set(incorrectAnswers)]; // Remove duplicates
+        const uniqueIncorrectAnswers = [...new Set(incorrectAnswers)]; // Remove duplicates
     
-    uniqueIncorrectAnswers.forEach((question, index) => {
-        recapContainer.innerHTML += `<p><strong>Question ${index + 1}:</strong> ${question}</p>`;
-    });
+        uniqueIncorrectAnswers.forEach((question, index) => {
+            recapContainer.innerHTML += `<p><strong>Question ${index + 1}:</strong> ${question}</p>`;
+        });
+    }
 }
